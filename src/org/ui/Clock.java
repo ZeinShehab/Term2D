ca65eaa421e29bcf9528f251b5eeace9d94499bd
ZeinShehab <zeinshehab@outlook.com>
2022-01-24 12:04:15 +0200

    Create game clock

---
+++ src/org/ui/Clock.java
@@ -0,0 +1,19 @@
+package org.ui;
+
+import java.util.concurrent.TimeUnit;
+
+public class Clock {
+    private final int FPS;
+
+    public Clock(int fps) {
+        this.FPS = fps;
+    }
+
+    public Clock() {
+        this.FPS = 30;
+    }
+
+    public void tick() throws InterruptedException {
+        TimeUnit.MICROSECONDS.sleep(1000*1000 / FPS);
+    }
+}
