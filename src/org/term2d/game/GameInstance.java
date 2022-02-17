da7ace06860b1b0fc0092b33d1222856d8bba0dc
ZeinShehab <zeinshehab@outlook.com>
2022-02-17 13:59:37 +0200

    Create GameInstance

---
+++ src/org/term2d/game/GameInstance.java
@@ -0,0 +1,67 @@
+package org.term2d.game;
+
+import java.util.ArrayList;
+
+import org.term2d.Clock;
+import org.term2d.display.Display;
+import org.term2d.geom.Shape;
+
+public class GameInstance {
+    ArrayList<GameObject> gameObjects;
+    public final Display display;
+    public final Clock clock;
+    
+    private boolean quit = false;
+
+    public GameInstance(int width, int height) {
+        gameObjects = new ArrayList<>();
+        display     = new Display(width, height);
+        clock       = new Clock();
+    }
+
+    private void startObjects() {
+        for (GameObject gameObject : gameObjects)
+            gameObject.start();
+    }
+
+    private void updateObjects() {
+        for (GameObject gameObject : gameObjects) {
+            if (gameObject instanceof Shape)
+                display.draw((Shape) gameObject);
+            gameObject.update();
+        }
+    }
+
+    public void quit() {
+        quit = true;
+    }
+
+    public void addObject(GameObject gameObject) {
+        gameObject.setGameInstance(this);
+        gameObjects.add(gameObject);
+    }
+
+    private void mainLoop() {
+        display.clearConsole();
+        display.hideCursor();
+        
+        startObjects();
+
+        while (!quit) {
+            display.clearDisplay();
+
+            updateObjects();
+
+            display.show();
+
+            clock.tick();
+        }
+        display.showCursor();
+    }
+
+    public void run() {
+        new Thread(() -> {
+            mainLoop();
+        }).start();
+    }
+}
