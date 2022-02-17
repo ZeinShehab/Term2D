ec62196b9ad1e933490e9dbb87abb37918f0ed2b
ZeinShehab <zeinshehab@outlook.com>
2022-02-17 13:58:52 +0200

    Add damping

---
+++ src/org/term2d/Physics.java
@@ -0,0 +1,6 @@
+package org.term2d;
+public class Physics {
+    public static final double GRAVITY = 200;
+    public static final double DT      = 1.0/60.0;
+    public static final double DAMP    = - 0.75;   
+}
