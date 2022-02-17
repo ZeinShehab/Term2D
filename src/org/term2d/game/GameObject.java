31bfd416dfaea552a30f5218ffc76cb583d6c564
ZeinShehab <zeinshehab@outlook.com>
2022-02-17 13:59:19 +0200

    Attach GameObject to GameInstance

---
+++ src/org/term2d/game/GameObject.java
@@ -0,0 +1,17 @@
+package org.term2d.game;
+
+public class GameObject {
+    protected GameInstance game;
+
+    protected void setGameInstance(GameInstance gameInstance) {
+        game = gameInstance;
+    }
+
+    public void start() {
+
+    }
+
+    public void update() {
+
+    }
+}
