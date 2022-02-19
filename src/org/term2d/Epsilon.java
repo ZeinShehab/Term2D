65e7e0b5bdd4d0c658563804aa43b267c99dc699
ZeinShehab <zeinshehab@outlook.com>
2022-02-19 16:17:40 +0200

    Create GlobalConstants

--- src/org/term2d/Epsilon.java
+++
@@ -1,5 +0,0 @@
-package org.term2d;
-
-public class Epsilon {
-    public static final double EPS = 1.0E-6;
-}
---
+++ src/org/term2d/GlobalConstants.java
@@ -0,0 +1,6 @@
+package org.term2d;
+
+public class GlobalConstants {
+    public static final double EPS        = 1.0E-3;
+    public static final double LINE_ERROR = 0.75;
+}
