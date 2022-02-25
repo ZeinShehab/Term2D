2b188dfd3316184debdbe69f2dcfa557ce644d47
ZeinShehab <zeinshehab@outlook.com>
2022-02-25 12:29:36 +0200

    Add tag for GameObjects

--- src/org/term2d/core/GameObject.java
+++ src/org/term2d/core/GameObject.java
@@ -1,7 +1,14 @@
 package org.term2d.core;
 
+
 public class GameObject {
+    public String tag;
     protected GameInstance game;
+
+    public GameObject() {
+        tag = "";
+        game = null;
+    }
 
     protected void setGameInstance(GameInstance gameInstance) {
         game = gameInstance;
